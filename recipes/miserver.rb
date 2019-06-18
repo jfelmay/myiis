0b85f289ea20f24ff1821480f2bd8bda5257f7b9
felix <jfel@example.com>
2019-06-18 19:36:41 +0000

    0.2.0: muestra informacion de la maquina

--- recipes/miserver.rb
+++ recipes/miserver.rb
@@ -9,6 +9,10 @@ end
 
 file 'C:\inetpub\wwwroot\Default.htm' do
   content "<h1>Hello, world!</h1>"
+          "<h2>PLATFORM: #{node["plataform"]}</h2>"
+          "<h3>HOSTNAME: #{node["hostname"]}</h3>"
+          "<h4>MEMORY: #{node["memory"]["total"]}</h4>"
+          "<h5>CPU Mhz: #{node["cpu"]["0"][mhz]}</h4>"
 end
 
 service "w3svc" do
